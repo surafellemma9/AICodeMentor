32f3f02ef7beef6eebaf5a0557a92b7482486a30
surafellemma9 <surafellemma9@gmail.com>
2025-07-24 18:02:48 -0400

    testing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Phase 1 : Setup & Project Skeleton
 
-* Surafel
+* Surafel123
 * Same 
 * Fidel
 
---
+++ templates/chatbot/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def ask_question(request):
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        code = request.POST.get('code')
+        response = f"Question: {question}\n\nCode:\n{code}"
+        return render(request, 'chatbot/response.html', {'response': response})
+    
+    return render(request, 'chatbot/form.html')
