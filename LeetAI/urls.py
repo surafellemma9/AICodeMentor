0e480da6b9d759f08d1631d2f6e4263cc011534b
surafellemma9 <surafellemma9@gmail.com>
2025-08-23 19:04:16 -0400

    test

--- LeetAI/urls.py
+++ LeetAI/urls.py
@@ -1,18 +1,6 @@
 # LeetAI/urls.py
-from django.urls import path
-
-from chatbot import views as cb
-
-app_name = "chatbot"
+from django.urls import path, include
 
 urlpatterns = [
-    path("chatbot/diag/", cb.diag, name="chatbot_diag"),
-    path("chatbot/gemini_probe/", cb.gemini_probe, name="chatbot_gemini_probe"),
-
-    # Main chat page (what your template renders)
-    path("chatbot/", cb.chat_page, name="chatbot_home"),
-    path("chatbot/new/", cb.new_chat, name="chatbot_new"),
-
-    # AJAX/form endpoint
-    path("chatbot/ask/", cb.submit_chat, name="chatbot_ask"),
+    path("chatbot/", include(("chatbot.urls", "chatbot"), namespace="chatbot")),
 ]
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,16 +1,19 @@
 # chatbot/urls.py
 from django.urls import path
-from django.urls import path, include
-from chatbot import views as cb
 
-from . import views
+from . import views as cb
 
 app_name = "chatbot"
 
 urlpatterns = [
-    path("", views.form_view, name="chatbot_home"),
-    path("ask/", views.submit_chat, name="ask"),
-    path("new/", views.new_chat, name="new"),
-    
+    # diagnostics
+    path("diag/", cb.diag, name="chatbot_diag"),
+    path("gemini_probe/", cb.gemini_probe, name="chatbot_gemini_probe"),
 
+    # main chat UI
+    path("", cb.chat_page, name="chatbot_home"),
+    path("new/", cb.new_chat, name="chatbot_new"),
+
+    # submit endpoint (AJAX or form)
+    path("ask/", cb.submit_chat, name="chatbot_ask"),
 ]
