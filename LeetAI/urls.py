c20b2579e22f7fcbbc314de2da01589b3cd255be
surafellemma9 <surafellemma9@gmail.com>
2025-08-23 19:21:45 -0400

    34

--- LeetAI/urls.py
+++ LeetAI/urls.py
@@ -1,6 +1,8 @@
-# LeetAI/urls.py
 from django.urls import path, include
 
 urlpatterns = [
     path("chatbot/", include(("chatbot.urls", "chatbot"), namespace="chatbot")),
+    # Optional: make site root land on chat
+    # from django.views.generic import RedirectView
+    # path("", RedirectView.as_view(pattern_name="chatbot:chatbot_home", permanent=False)),
 ]
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,19 +1,13 @@
-# chatbot/urls.py
 from django.urls import path
-
 from . import views as cb
 
 app_name = "chatbot"
 
 urlpatterns = [
-    # diagnostics
     path("diag/", cb.diag, name="chatbot_diag"),
     path("gemini_probe/", cb.gemini_probe, name="chatbot_gemini_probe"),
 
-    # main chat UI
     path("", cb.chat_page, name="chatbot_home"),
     path("new/", cb.new_chat, name="chatbot_new"),
-
-    # submit endpoint (AJAX or form)
     path("ask/", cb.submit_chat, name="chatbot_ask"),
 ]
