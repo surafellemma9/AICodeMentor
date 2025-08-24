bcce1d5e050a03fa0b7fec606e5ef39b61897e8a
surafellemma9 <surafellemma9@gmail.com>
2025-08-24 00:36:56 -0400

    Fix: remove/restore gemini_probe route to resolve deploy error

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,3 +1,4 @@
+# chatbot/urls.py
 from django.urls import path
 
 from . import views as cb
@@ -5,12 +6,7 @@ from . import views as cb
 app_name = "chatbot"
 
 urlpatterns = [
-    path("diag/", cb.diag, name="chatbot_diag"),
-    path("gemini_probe/", cb.gemini_probe, name="chatbot_gemini_probe"),
-
-    # 👇 This is the ChatGPT-style page
     path("", cb.chat_page, name="chatbot_home"),
-
     path("new/", cb.new_chat, name="chatbot_new"),
     path("ask/", cb.submit_chat, name="chatbot_ask"),
 ]
