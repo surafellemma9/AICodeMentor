55ce2276af1e0e25eb34a305ad7c8b0b644b5871
surafellemma9 <surafellemma9@gmail.com>
2025-08-27 20:30:07 -0400

    form.html fixing send button

--- LeetAI/urls.py
+++ LeetAI/urls.py
@@ -1,14 +1,9 @@
-# LeetAI/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # Chatbot app (namespaced)
     path("chatbot/", include(("chatbot.urls", "chatbot"), namespace="chatbot")),
-
-    # Redirect root "/" to chatbot home by route name (safer than hardcoding URL)
     path("", RedirectView.as_view(pattern_name="chatbot:chatbot_home", permanent=False)),
 ]
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,5 +6,5 @@ app_name = "chatbot"
 urlpatterns = [
     path("", cb.chat_page, name="chatbot_home"),
     path("new/", cb.new_chat, name="chatbot_new"),
-    path("ask/", cb.submit_chat, name="chatbot_ask"),
+    path("ask/", cb.submit_chat, name="chatbot_ask"),   # ← this name must match the form action
 ]
